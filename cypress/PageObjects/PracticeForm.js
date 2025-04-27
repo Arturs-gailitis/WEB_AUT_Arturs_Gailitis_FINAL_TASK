ac1bd70f37efa4e453e95f92a5786d4fe16b391f
Arturs-gailitis <artursgailitis2003@gmail.com>
2025-04-27 09:14:17 +0300

    commit_2

--- cypress/PageObjects/PracticeForm.js
+++ cypress/PageObjects/PracticeForm.js
@@ -4,4 +4,28 @@ export class PracticeForm extends BasePage {
     static get url() {
         return 'automation-practice-form';
     }
+
+    static get clickCalendar() {
+        return cy.get('input#dateOfBirthInput');
+    }
+
+    static get selectYear() {
+        return cy.get('select.react-datepicker__year-select');
+    }
+
+    static get selectMonth() {
+        return cy.get('select.react-datepicker__month-select');
+    }
+
+    static get selectDay() {
+        return cy.get('div.react-datepicker__day--028');
+    }
+
+    static get setSubject() {
+        return cy.get('input#subjectsInput');
+    }
+
+    static get setHobbie() {
+        return cy.get('div.custom-control');
+    }
 }
--- cypress/e2e/demoqa.cy.js
+++ cypress/e2e/demoqa.cy.js
@@ -6,5 +6,19 @@ describe('demoqa scenario', () => {
     
     //Open https://demoqa.com/automation-practice-form
     PracticeForm.visit();
+
+    //Set the- Date of Birth- with Calendar widget to- 28th of February, 1930.
+    PracticeForm.clickCalendar.click();
+    PracticeForm.selectYear.select('1930');
+    PracticeForm.selectMonth.select('February');
+    PracticeForm.selectDay.last().click();
+
+    //Set Subjects to Economics.
+    PracticeForm.setSubject.type('Economics');
+
+    //Set Hobbies to Music.
+    PracticeForm.setHobbie.eq(2).click();
+    //Input all the necessary information in the text fields, buttons, etc, where it is not stated otherwise.
+    
   })
 })
